a7084a9b1a5034c562b12daff5e5fa823a28c4ff
kaleb.silva <kaleb.silva@itbam.org.br>
2021-04-08 14:33:17 -0400

    feat:implemented  management state

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -3,13 +3,21 @@ import 'package:app_request_api/src/repositories/todo_repositoty.dart';
 
 class HomeController {
   List<TodoModel> todos = [];
-
   final TodoRepository _repository;
+  HomeState state = HomeState.start;
 
   HomeController([TodoRepository repository])
       : _repository = repository ?? TodoRepository();
 
   Future start() async {
-    todos = await _repository.fetchTodos();
+    state = HomeState.loading;
+    try {
+      todos = await _repository.fetchTodos();
+      state = HomeState.success;
+    } catch (e) {
+      state = HomeState.error;
+    }
   }
 }
+
+enum HomeState { start, loading, success, error }
