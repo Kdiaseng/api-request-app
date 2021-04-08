9780c01104af1a47c3eb4925dd5acf6cff7d96cd
kaleb.silva <kaleb.silva@itbam.org.br>
2021-04-08 13:22:42 -0400

    refactor:using mockito to test get list

--- lib/src/repositories/todo_repositoty.dart
+++ lib/src/repositories/todo_repositoty.dart
@@ -2,8 +2,17 @@ import 'package:app_request_api/src/models/todo_model.dart';
 import 'package:dio/dio.dart';
 
 class TodoRepository {
-  final dio = Dio();
+  Dio dio;
   final url = 'https://jsonplaceholder.typicode.com/todos/';
+
+  TodoRepository([Dio client]) {
+    // não é obrigatório
+    if (client == null) {
+      this.dio = Dio();
+    } else {
+      this.dio = client;
+    }
+  }
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
